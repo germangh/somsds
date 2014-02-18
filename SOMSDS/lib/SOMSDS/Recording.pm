92ccefef674a00d0bcae4108326abb99f5a4c038
German Gomez-Herrero <g@germangh.com>
2014-02-18 11:27:32 +0100

    All files owned by root

--- SOMSDS/lib/SOMSDS/Recording.pm
+++ SOMSDS/lib/SOMSDS/Recording.pm
@@ -48,14 +48,14 @@ my ($vol, $dir, $file) = File::Spec->splitpath($0);
 #print "$vol --- $dir --- $file\n"; die;
 my $somsds_file;
 if ($vol){
-  $somsds_file        = abs_path catfile($vol, $dir, 'SOMSDS.ini'); 
+  $somsds_file        = abs_path catfile($vol, $dir, 'SOMSDS.ini');
 } else {
-  $somsds_file        = abs_path catfile($dir, 'SOMSDS.ini'); 
+  $somsds_file        = abs_path catfile($dir, 'SOMSDS.ini');
 }
 
-unless (-e $somsds_file){  
-  $somsds_file = '/etc/SOMSDS.ini'; 
-} 
+unless (-e $somsds_file){
+  $somsds_file = '/etc/SOMSDS.ini';
+}
 unless (-e $somsds_file){
   croak "I could not find the configuration file $somsds_file\n";
 }
@@ -72,9 +72,9 @@ sub new {
   my %fields = (
     id          => undef,
     root_path   => undef,
-    archived    => undef,   
+    archived    => undef,
     file        => {},
-    subject     => {},  
+    subject     => {},
     modality    => {},
     condition   => {},
     technique   => {},
@@ -83,37 +83,37 @@ sub new {
     block       => {},
     settings       => undef,
     settings_file  => undef,
-    meta        => {} 
+    meta        => {}
   );
-  my $self = {    
+  my $self = {
     _permitted      => \%fields,
     %fields,
-  };    
-  
-  # Load recording settings 
+  };
+
+  # Load recording settings
   unless ($settings_file && -e $settings_file){
     die "I could not find the recording settings file $settings_file\n";
   }
 
   $self->{settings} = new Config::IniFiles(-file => $settings_file);
-  
+
   $self->{settings_file} = $settings_file;
   my @recid = $self->{settings}->GroupMembers('recording');
   unless (@recid){
     croak "Recording ID is missing from file $settings_file\n";
   }
-  
+
   if (scalar @recid > 1){
     croak "Found multiple recording IDs in $settings_file: ", @recid, "\n";
   }
   $self->{id} = shift @recid;
   $self->{id} =~ s/recording\s+//;
-  
+
   # Read recording meta-information
   my $sec = 'recording '.$self->{id};
   foreach ($self->{settings}->Parameters($sec)){
     $self->{meta}->{$_} = $self->{settings}->val($sec, $_);
-  }  
+  }
 
   # Load recording settings
   for my $group (qw(subject modality device technique device session block condition)){
@@ -121,7 +121,7 @@ sub new {
 
       if ((any {$_ eq $group} qw(modality device technique)) &&
                 !$somsds_cfg->SectionExists("$group_member")){
-        croak 
+        croak
         "$group_member is not listed in $somsds_file\n".
         "Listed values of field ''$group'' are:\n".
         join(', ', $self->{settings}->GroupMembers($group));
@@ -129,24 +129,24 @@ sub new {
       my $group_member_name = $group_member;
       $group_member_name =~ s/$group\s+//;
       $self->{$group}->{$group_member_name} = {};
-      for my $param ($self->{settings}->Parameters("$group_member")){     
-        $self->{$group}->{$group_member_name}->{$param} = 
+      for my $param ($self->{settings}->Parameters("$group_member")){
+        $self->{$group}->{$group_member_name}->{$param} =
           $self->{settings}->val("$group_member", $param);
       }
     }
   }
-  
-  
+
+
   # There must be at least one modality and one subject
   for (qw(modality subject)){
     unless (keys %{$self->{$_}}){
       croak "At least one $_ must be specified in $settings_file\n";
     }
-  }    
-  
+  }
+
   bless $self, $class;
 
-  # Create the folder structure  
+  # Create the folder structure
   if (exists($options->{tmp}) && $options->{tmp}) {
 	  $self->root_path(catdir(cwd(), $self->{id}));
 
@@ -154,11 +154,11 @@ sub new {
     # The root path of the recording
     $self->root_path(
                   catdir($somsds_cfg->val('somsds', 'root_path'),
-                         $somsds_cfg->val('somsds', 'rec_folder'), 
+                         $somsds_cfg->val('somsds', 'rec_folder'),
                          $self->{id})
                 );
   }
- 
+
   # If there are multiple input ids
   if (@_){
     my @self = ($self);
@@ -167,10 +167,10 @@ sub new {
       @self = push @self, $new_rec;
     }
     return @self;
-  }  
-  
+  }
+
   return $self;
-}  
+}
 
 
 #################################
@@ -210,31 +210,31 @@ sub import_dir {
 
   my $conf = new Config::IniFiles(-file => $conf_file);
 
-  my $inc_regex = $conf->val('include', 'regexp'); 
-  my $exc_regex = $conf->val('exclude', 'regexp');	
+  my $inc_regex = $conf->val('include', 'regexp');
+  my $exc_regex = $conf->val('exclude', 'regexp');
 
   if ($fuse){
     # Copy $dir into recordings/$rec/
     dircopy($dir, $self->{root_path});
-  } else {  
+  } else {
     if ($inc_regex) {$inc_regex =~ s/^"(.+)"$/$1/;}
     if ($exc_regex) {$exc_regex =~ s/^"(.+)"$/$1/;}
 
-    my $base_import_dir = catdir( $self->{root_path}, 
-                                  $somsds_cfg->val('somsds', 
+    my $base_import_dir = catdir( $self->{root_path},
+                                  $somsds_cfg->val('somsds',
                                   'import_folder'));
 
-    $conf_file = fileparse($conf_file, qr/\.[^.]*/); 
-    my $import_dir  = catdir($base_import_dir, 
+    $conf_file = fileparse($conf_file, qr/\.[^.]*/);
+    my $import_dir  = catdir($base_import_dir,
                              $conf_file."_".$time{'yyyy-mm-dd_hh-mm-ss'});
-    # Copy $dir into $rec/imported/$date/ 
+    # Copy $dir into $rec/imported/$date/
     if (!$inc_regex && !$exc_regex){
       print "$dir\n--->>$import_dir\n";
-      dircopy($dir, $import_dir); 
-      finddepth(sub{rmdir}, $import_dir);     
+      dircopy($dir, $import_dir);
+      finddepth(sub{rmdir}, $import_dir);
     } else {
-      my $count=0;    
-      find(sub 
+      my $count=0;
+      find(sub
   	  {
   	if ($inc_regex) {return unless ($File::Find::name =~ m/$inc_regex/);}
         if ($exc_regex) {return if ($File::Find::name =~ m/$exc_regex/);}
@@ -242,28 +242,28 @@ sub import_dir {
 
         # Copy the file to the new location
         my $import_file = $File::Find::name;
-        $import_file =~ s&$dir&$import_dir&;   
-             
+        $import_file =~ s&$dir&$import_dir&;
+
         print "$File::Find::name \n---> $import_file\n\n";$|++;
-  
+
         if (-d $File::Find::name){
           make_path $import_file
         }else{
           $import_file =~ m%(.+)/[^/]+$%;
-          if ($1) {make_path $1;}  
+          if ($1) {make_path $1;}
             `cp $File::Find::name $import_file`;# || die "$!";
-        }    
-   
+        }
+
         chmod (0755, $import_file);
   	  },
       $dir);
-      print "$count files were copied int $import_dir\n";             
+      print "$count files were copied int $import_dir\n";
     } # endif
     # Make the import dir inmutable
     finddepth(sub
         {
-           system("chattr +i $File::Find::name");    
-        }, $import_dir); 
+           system("chattr +i $File::Find::name");
+        }, $import_dir);
     print "made inmutable $import_dir'\n";
   }
 
@@ -276,7 +276,7 @@ sub import_dir {
 
 sub root_path {
   my ($self, $path) = (shift, shift);
-  if ($path) {    
+  if ($path) {
     $self->{root_path} = $path;
     return $self;
   }
@@ -289,12 +289,12 @@ sub protect {
   #}
   my $self = shift;
   my $dir = catdir($self->root_path(), 'import');
-  find(sub 
+  find(sub
   	{
   		return if ($File::Find::name =~ m%import/00%);
 		chmod 0555, $File::Find::name;
 		my $cmd = "chattr -i \"$File::Find::name\"";
-		print "$cmd\n";	
+		print "$cmd\n";
 		system($cmd);
 		$cmd = "chattr +a \"$File::Find::name\"";
 		print "$cmd\n";
@@ -315,7 +315,7 @@ sub protect {
 sub unprotect {
   my $self = shift;
   my $dir = catdir($self->root_path(), 'import');
-  find(sub 
+  find(sub
   	{
  		return if (-l $File::Find::name);
 		chmod 0755, $File::Find::name;
@@ -340,7 +340,7 @@ sub make_folders {
   if (exists $self->{meta}->{responsible}){
     $responsible = $self->{meta}->{responsible};
     $responsible = getpwnam($responsible);
-  }  
+  }
 
   for (qw(subject modality condition device session block)){
 		unless (keys %{$self->{$_}}){
@@ -348,32 +348,32 @@ sub make_folders {
 		}
 	}
   my $path;
-  
+
   for my $subjid (keys %{$self->{subject}}){
     for my $modid (keys %{$self->{modality}}){
       for my $condid (keys %{$self->{condition}}){
         for my $devid (keys %{$self->{device}}){
           for my $sessid (keys %{$self->{session}}){
-            for my $blkid (keys %{$self->{block}}){              
+            for my $blkid (keys %{$self->{block}}){
               $path         = $somsds_cfg->val('recording','folders');
               $path         =~ s/SUBJID/$subjid/g;
               $path         =~ s/MODID/$modid/g;
               $path         =~ s/CONDID/$condid/g;
-              $path         =~ s/CONDID/$devid/g;              
+              $path         =~ s/CONDID/$devid/g;
               $path         =~ s/SESSID/$sessid/g;
               $path         =~ s/BLKID/$blkid/g;
               my @paths = split("\n", $path);
-            
+
               foreach (@paths){
-                my $options   = { mode => 0755, verbose => 1, 
+                my $options   = { mode => 0755, verbose => 1,
                                   owner => $responsible};
-                my $new_path  = catdir($self->root_path(), split(/\s+/, $_));                      
+                my $new_path  = catdir($self->root_path(), split(/\s+/, $_));
 				unless (-e $new_path){
-	              	make_path $new_path, $options;  
-					#print "mkdir $new_path\n";                                             
+	              	make_path $new_path, $options;
+					#print "mkdir $new_path\n";
 				}
-                
-								
+
+
               }
             }
           }
@@ -381,8 +381,6 @@ sub make_folders {
       }
     }
   }
-
-  chown $responsible, -1, $self->root_path();
 }
 
 
@@ -397,7 +395,7 @@ sub prune {
 	  finddepth(sub{rmdir;}, $dir_tree);
 	} else {
     finddepth(sub{
-                  unless (m%(doc/|raw/)%){     
+                  unless (m%(doc/|raw/)%){
                   rmdir;
                   }
                  }, $dir_tree);
@@ -416,19 +414,19 @@ sub clear_links {
         unlink $File::Find::name;
         print "unlink $File::Find::name\n";
       }
-    }, 
+    },
     catdir($self->{root_path}, 'subjects'));
 }
 
 #################################
 
-sub save {  
+sub save {
   my ($self, $somsds_obj) = (shift, shift);
   my $rec_table = $somsds_obj->{'rec_csv'};
   $rec_table =~ s/\..*$//;
   my $files_table = $somsds_obj->{'files_csv'};;
   $files_table =~ s/\..*$//;
-  
+
   # Remove the recording from the table if it is already there
   my $rec_id = $self->{id};
   print "\n\nDeleting links from recording $rec_id...";
@@ -440,24 +438,24 @@ sub save {
   $query = "INSERT INTO $rec_table VALUES (?,?,?,?,?,?)";
   my $uname = getpwuid( $< );
   my @entry = ( $rec_id,
-                $uname, 
+                $uname,
                 $time{'yyyy/mm/ddThh:mm:ss'},
                 $self->{archived},
                 $self->{settings_file},
                 meta_as_string($self->{meta}),
               );
-  $somsds_obj->sql($query, undef, @entry);        
+  $somsds_obj->sql($query, undef, @entry);
   print "[done]\n\n";
-  
+
   # Insert files
   my $counter=0;
   my $num_files = keys %{$self->{file}};
-  my $num_files_by100 = POSIX::ceil($num_files/100);    
-  $|=1; 
+  my $num_files_by100 = POSIX::ceil($num_files/100);
+  $|=1;
   print "Deleting $num_files files from recording $rec_id...";
   # Remove all files from this recording
   $query = "DELETE FROM $files_table WHERE recording='$rec_id'";
-  $somsds_obj->sql($query); 
+  $somsds_obj->sql($query);
 
   print "[done]\n\n";
 
@@ -467,8 +465,8 @@ sub save {
       unless ($counter % $num_files_by100 ) {
         my $pctg = POSIX::ceil(100*$counter/$num_files);
           print "Inserting files of recording '$rec_id' to the DB...$pctg%\r";
-        $|++;  
-      }      
+        $|++;
+      }
     }
     print "\n";
 }
@@ -485,15 +483,15 @@ sub file {
 
 sub AUTOLOAD {
   my $self = shift;
-  my $type = ref($self) or 
+  my $type = ref($self) or
     croak "$self is not an object\n";
-    
+
   my $name = $AUTOLOAD;
   $name =~ s/.*://;
   unless (exists $self->{_permitted}->{$name}){
     croak "Can't access $name field in class $type\n";
   }
-  
+
   if (@_){
     return $self->{$name} = shift;
   } else {
@@ -509,15 +507,15 @@ sub _class_autoload {
 
   unless (exists $self->{_permitted}->{$name}) {
     croak "Can't access '$name' field in class $type";
-  }  
+  }
 
   OBJECT: while (my $value = shift) {
     if (ref($value) eq $force_type) {
       my $id = $value->id() || next OBJECT;
-      $self->{$name}->{$id} = $value;      
+      $self->{$name}->{$id} = $value;
     } else {
-      croak "$self is not an object of class $force_type!"    
-    }    
+      croak "$self is not an object of class $force_type!"
+    }
   }
 
   return $self->{$name};
